924f2c039bb0d5003a57969e3aa62ea099ecd610
karukador <152086077+karukador@users.noreply.github.com>
2024-04-12 20:38:13 +0300

    Create system_config.py

---
+++ system_config.py
@@ -0,0 +1,5 @@
+URL = "https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize" 
+voice = "filipp" # желаемы голос можно выбрать в списке - https://yandex.cloud/ru/docs/speechkit/tts/voices
+admin_id = ВАШ_TELEGRAM_ID
+MAX_USER_TTS_SYMBOLS = 500
+MAX_TTS_SYMBOLS = 2000
