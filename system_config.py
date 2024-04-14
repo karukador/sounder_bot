e47c985df63856e753897d37e270dd52e1a866af
karukador <152086077+karukador@users.noreply.github.com>
2024-04-14 20:33:37 +0300

    Update system_config.py

--- system_config.py
+++ system_config.py
@@ -1,5 +1,10 @@
-URL = "https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize" 
-voice = "filipp" # желаемы голос можно выбрать в списке - https://yandex.cloud/ru/docs/speechkit/tts/voices
-admin_id = ВАШ_TELEGRAM_ID
+URL = "https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize"
+VOICE = "filipp"  # желаемый голос можно выбрать в списке - https://yandex.cloud/ru/docs/speechkit/tts/voices
+LANGUAGE = "ru-RU"
+
+CONTENT_TYPES = ["text", "audio", "document", "photo", "sticker", "video", "video_note", "voice"]
+ADMIN_ID: int = 1234
 MAX_USER_TTS_SYMBOLS = 500
 MAX_TTS_SYMBOLS = 2000
+
+DB_NAME = "speech_kit.db"
